fb158023bdf9a76b4ad7d8f1a3e6b08e080aab18
Vyacheslav <slava_94@mail.ru>
2023-01-16 21:05:04 +0300

    getting a list of all employees and work with *Postman* application

---
+++ src/main/java/com/simakov/spring/rest/controller/MeRestController.java
@@ -0,0 +1,25 @@
+package com.simakov.spring.rest.controller;
+
+import com.simakov.spring.rest.entity.Employee;
+import com.simakov.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MeRestController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees(){
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        return allEmployees;
+    }
+
+}
