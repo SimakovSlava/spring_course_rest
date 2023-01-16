4e594bebceb301ddd17c91f84f5ab456deb91f6c
Vyacheslav <slava_94@mail.ru>
2023-01-16 21:13:48 +0300

    getting a single worker

--- src/main/java/com/simakov/spring/rest/controller/MeRestController.java
+++ src/main/java/com/simakov/spring/rest/controller/MeRestController.java
@@ -4,6 +4,7 @@ import com.simakov.spring.rest.entity.Employee;
 import com.simakov.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +23,10 @@ public class MeRestController {
         return allEmployees;
     }
 
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id){
+        Employee employee = employeeService.getEmployee(id);
+        return employee;
+    }
 }
